9aee0e216596081d6821f4ed6ffc6460a222e4b4
1584071403@qq.com <Ljh30303>
2019-07-24 20:21:20 +0800

    5th

--- manage/src/main/java/org/gavin/controller/PicController.java
+++ manage/src/main/java/org/gavin/controller/PicController.java
@@ -3,7 +3,6 @@ package org.gavin.controller;
 import org.gavin.service.PicService;
 import org.gavin.vo.EasyUI_Image;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
