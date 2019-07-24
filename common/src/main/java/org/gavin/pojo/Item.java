585d7561a95d6d3c44c096d8814bd2270e4ce435
1584071403@qq.com <Ljh30303>
2019-07-24 11:26:47 +0800

    First

--- common/src/main/java/org/gavin/pojo/Item.java
+++ common/src/main/java/org/gavin/pojo/Item.java
@@ -1,5 +1,24 @@
 package org.gavin.pojo;
 
-public class Item {
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+@TableName("tb_item")
+public class Item extends BasePojo{
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    private String title;
+    private String sell_point;
+    private Long price;
+    private Integer num;
+    private String barcode;
+    private String image;
+    private Long cid;
+    private Integer status;
 
 }
